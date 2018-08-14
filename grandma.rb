cf4bead70d63528bd213475a513788ac365e8c8d
John Grubbs <johnmlowery@gmail.com>
2018-08-14 15:31:54 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,6 @@
 def speak_to_grandma
-  puts "I love you too Pumpkin".upper
+  if ("I love you grandma".upper)
+ return puts "I love you too Pumpkin".upper
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
