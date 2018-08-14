f1e0c65db829a8a7ec8a92178f403bd57a19b5a4
John Grubbs <johnmlowery@gmail.com>
2018-08-14 16:02:27 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase = "I LOVE YOU GRANDMA!")
-    if phrase == "I LOVE YOU GRANDMA!"
-      puts "I LOVE YOU TOO PUMKIN!"
+    if (phrase == "I LOVE YOU GRANDMA!"){
+      puts "I LOVE YOU TOO PUMKIN!"}
     else
       puts "HUH?! SPEAK UP, SONNY!"
   end
